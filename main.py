ea0f4faf6d8d5f678fa5889d4346b9944093322d
Stepan Shabalin <stomperhomp@gmail.com>
2023-11-21 22:58:52 -0500

    Almost everything ready for training, just need to change attention

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from apex.transformer.pipeline_parallel import get_forward_backward_func, build_model
+from apex.transformer.pipeline_parallel.utils import setup_microbatch_calculator
 from apex.transformer import parallel_state, tensor_parallel
+from apex.contrib.optimizers.distributed_fused_adam import DistributedFusedAdam
+from apex.optimizers.fused_adam import FusedAdam
+
 from model import ModelArgs, Transformer, PipelineStage
+
 from pathlib import Path
 import argparse
 import torch
@@ -23,6 +28,17 @@ def set_random_seed(seed: int):
     torch.manual_seed(seed)
     tensor_parallel.model_parallel_cuda_manual_seed(seed)
 
+
+# I don't understand why all the extra stuff will needed so I'll run it to see why
+def loss_fn(pred, label):
+    return vocab_parallel_cross_entropy(pred, label).mean(), {}
+    
+    
+def train_step(batch, model):
+    out = model(batch[:, :-1], start_pos=0)
+    label = batch[:, 1:]
+    return out, lambda pred: loss_fn(pred, label)
+    
 
 def main(llama_path=Path("llama-2-7b")):
     rank = int(os.environ["RANK"])
@@ -68,12 +84,52 @@ def main(llama_path=Path("llama-2-7b")):
                         virtual_pipeline_model_parallel_size,
                         args=llama_args)
     
+    global_batch_size = 64
+    micro_batch_size = 4
+    setup_microbatch_calculator(
+        rank=rank,
+        rampup_batch_size=None,
+        global_batch_size=global_batch_size,
+        micro_batch_size=micro_batch_size,
+        data_parallel_size=data_parallel_size,
+    )
+    
     state_dict = torch.load(str(llama_path / "consolidated.00.pth"), mmap=True)
     state_dict = {"module.wrapped." + k: v for k, v in state_dict.items()}
     state_dict = {k: state_dict[k] for k in models[0].state_dict().keys()}
     models[0].load_state_dict(state_dict)
     del state_dict
     
+    lr = 1e-4
+    weight_decay = 1e-5
+    optimizer = DistributedFusedAdam(
+        models[0].parameters(),
+        lr=lr,
+        weight_decay=weight_decay,
+        process_group=parallel_state.get_data_parallel_group(),
+        dtype=torch.bfloat16,
+        # TODO distribute over DP group?
+        # distributed_process_group=torch.distributed.new_group(ranks=[torch.distributed.get_rank()]),
+        # redundant_process_group=parallel_state.get_data_parallel_group(),
+        store_params=False,
+    )
+    
+    batch = torch.randint(0, vocab_size, (global_batch_size // data_parallel_size, llama_args.max_seq_len), device="cuda")
+    optimizer.zero_grad()
+    loss = forward_backward_func(
+        train_step,
+        batch,
+        models,
+        forward_only=False,
+        # IO shape?
+        tensor_shape=(llama_args.max_seq_len, micro_batch_size, llama_args.dim),
+        # T4 doesn't have bfloat16
+        dtype=torch.float16,
+        async_comm=True,
+        sync_batch_comm=False,
+        sequence_parallel_enabled=False,
+    )
+    optimizer.step()
     torch.distributed.barrier()
 
 
