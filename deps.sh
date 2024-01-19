992399abca06bfc4646367a26051d582035af5d4
neverix <nev@neverix.io>
2024-01-19 14:52:08 +0000

    Soon this will be fp8

--- deps.sh
+++ deps.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
 python -m pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
 if [ ! -d apex ] ; then
     git clone https://github.com/NVIDIA/apex
 fi
 cd apex
 git checkout 2386a912164b0c5cfcd8be7a2b890fbac5607c82
 python -m pip install --disable-pip-version-check --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" --config-settings "--build-option=--distributed_adam" --config-settings "--build-option=--deprecated_fused_adam" ./
+cd ..
+
+if [ ! -d TransformerEngine ] ; then
+    git clone --branch stable --recursive https://github.com/NVIDIA/TransformerEngine.git
+fi
+cd TransformerEngine
+export NVTE_FRAMEWORK=pytorch   # Optionally set framework
+python -m pip install .                   # Build and install
+cd ..
+
 python -m pip install -q mosaicml-streaming sentencepiece pandas pyarrow fire wandb
